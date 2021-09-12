d44d00e57ebfb9d727755e38faa1d89707d69092
airD173 <76494658+airD173@users.noreply.github.com>
2021-09-12 15:04:13 -0400

    Added dark mode toggle

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,10 @@ author = 'daspartho'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx_rtd_dark_mode']
+
+# user starts in dark mode
+default_dark_mode = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
